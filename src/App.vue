e2c499491b339fd482038ec6b75ac735fe7e80ba
Arm Jakkit <thekingnight98@gmail.com>
2023-12-12 09:02:50 +0700

    improve code

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,112 @@
 <template>
   <div class="container">
-    <img
-      class="image-rugby"
-      src="../src/assets/rugby-desktop.svg"
-      alt="rugby "
-    />
-    <div class="tab-title">
-      <div class="title">ATHLETS</div>
+    <div v-if="isMobile" class="tab-title">
+      <div class="title">{{ title1 }}</div>
     </div>
-    <div class="tab1">
+    <img :src="image1" class="image-rugby" alt="rugby" />
+    <div v-if="!isMobile" class="tab-title">
+      <div class="title">{{ title1 }}</div>
+    </div>
+    <div
+      v-for="(item, index) in part1"
+      :key="index"
+      :class="'tab' + (index + 1)"
+    >
       <div class="flex-centent1">
         <div class="label">
-          01
+          {{ item.number }}
           <p class="attache"></p>
         </div>
-        <div class="label-name">CONNECTION</div>
+        <div class="label-name">{{ item.label }}</div>
       </div>
       <div class="desc">
-        Connect with coaches directly, you can ping coaches to view profile.
+        {{ item.desc }}
       </div>
     </div>
-    <div class="tab2">
-      <div class="flex-centent1">
-        <div class="label">
-          02
+
+    <!-- part2 -->
+    <div v-if="isMobile" class="tab-title">
+      <div class="title">{{ title1 }}</div>
+    </div>
+    <img :src="image2" class="image-basketball" alt="basketball" />
+    <div v-if="!isMobile" class="tab-title">
+      <div class="title-part2">{{ title2 }}</div>
+    </div>
+    <div
+      v-for="(item, index) in part2"
+      :key="index"
+      :class="'tab' + (index + 1) + '-part2'"
+    >
+      <div class="flex-centent2">
+        <div :class="label">
+          {{ item.number }}
           <p class="attache"></p>
         </div>
-        <div class="label-name">COLLABORATION</div>
-      </div>
-      <div class="desc">
-        Work with other student athletes to increase visability. When you share
-        and like other players videos it will increase your visability as a
-        player. This is the team work aspect to Surface 1.
-      </div>
-    </div>
-    <div class="tab3">
-      <div class="flex-centent1">
-        <div class="label">
-          03
-          <p class="attache-white"></p>
-        </div>
-        <div class="label-name">GROWTH</div>
+        <div class="label-name">{{ item.label }}</div>
       </div>
       <div class="desc-white">
-        Resources and tools for you to get better as a student Athelte. Access
-        to training classes, tutor sessions, etc
-      </div>
-    </div>
-    <!-- part2  -->
-    <img
-      class="image-basketball"
-      src="../src/assets/basketball-desktop.svg"
-      alt="basketball "
-    />
-    <div class="tab-title">
-      <div class="title-part2">PLAYERS</div>
-    </div>
-    <div class="tab1-part2">
-      <div class="flex-centent2">
-        <div class="label">
-          01
-          <p class="attache"></p>
-        </div>
-        <div class="label-name">CONNECTION</div>
-      </div>
-      <div class="desc">
-        Connect with talented athlete directly, you can watch their skills
-        through video showreels directly from Surface 1.
-      </div>
-    </div>
-    <div class="tab2-part2">
-      <div class="flex-centent2">
-        <div class="label">
-          02
-          <p class="attache"></p>
-        </div>
-        <div class="label-name">COLLABORATION</div>
-      </div>
-      <div class="desc">
-        Work with recruiter to increase your chances of finding talented
-        athlete.
-      </div>
-    </div>
-    <div class="tab3-part2">
-      <div class="flex-centent2">
-        <div class="label-bottom">
-          03
-          <p class="attache-white"></p>
-        </div>
-        <div class="label-name">GROWTH</div>
-      </div>
-      <div class="desc-white">
-        Save your time, recruit proper athlets for your team.
+        {{ item.desc }}
       </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "App",
 
   components: {},
-
+  mounted() {
+    this.checkIfMobile();
+    window.addEventListener("resize", this.checkIfMobile);
+  },
+  methods: {
+    checkIfMobile() {
+      this.isMobile = window.innerWidth <= 767; // กำหนดขนาดเป็น mobile size ตามต้องการ
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.checkIfMobile);
+  },
   data: () => ({
-    //
+    isMobile: false,
+    title1: "ATHLETS",
+    title2: "PLAYERS",
+    image1: require("../src/assets/rugby-desktop.svg"),
+    image2: require("../src/assets/basketball-desktop.svg"),
+    part1: [
+      {
+        number: "01",
+        label: "CONNECTION",
+        desc: "Connect with coaches directly, you can ping coaches to view profile.",
+      },
+      {
+        number: "02",
+        label: "COLLABORATION",
+        desc: "Work with other student athletes to increase visability. When you share and like other players videos it will increase your visability as a player. This is the team work aspect to Surface 1.",
+      },
+      {
+        number: "03",
+        label: "GROWTH",
+        desc: "Resources and tools for you to get better as a student Athelte. Access to training classes, tutor sessions, etc ",
+      },
+    ],
+    part2: [
+      {
+        number: "01",
+        label: "CONNECTION",
+        desc: "Connect with talented athlete directly, you can watch their skills through video showreels directly from Surface 1.",
+      },
+      {
+        number: "02",
+        label: "COLLABORATION",
+        desc: "Work with recruiter to increase your chances of findingtalented athlete.",
+      },
+      {
+        number: "03",
+        label: "GROWTH",
+        desc: "Save your time, recruit proper athlets for your team. ",
+      },
+    ],
   }),
 };
 </script>
@@ -122,11 +124,6 @@ html {
   position: relative;
   width: 100%;
   height: 100vh;
-}
-
-.image-rugby {
-  position: absolute;
-  z-index: 2;
 }
 .flex-centent1 {
   display: flex;
@@ -232,7 +229,7 @@ html {
   background-color: #090c35;
   height: 238px;
 }
-.label-bottom {
+.label-part2 {
   color: #8f6be8;
   font-family: Roboto;
   font-size: 18px;
@@ -249,75 +246,80 @@ html {
   font-weight: 400;
   line-height: 28px;
 }
-.image-basketball {
-  position: absolute;
-  z-index: 2;
-}
 /* close tab3 */
 
-/* Media Queries เพื่อปรับแต่งสไตล์ตามขนาดหน้าจอที่ต่างกัน */
-@media only screen and (min-width: 1920px) {
-  /* ปรับแต่งสไตล์สำหรับขนาดหน้าจอ 1920px */
-  .title {
-    padding-left: 1008px;
-  }
-  .tab1 {
-    padding-left: 1008px;
-  }
-  .tab2 {
-    padding-left: 1008px;
-  }
-  .tab3 {
-    padding: 60px 0px 60px 1008px;
-  }
-  .tab1-part2 {
-    padding-left: 322px;
-  }
-  .tab2-part2 {
-    padding-left: 322px;
-  }
-  .tab3-part2 {
-    padding: 60px 0px 108px 322px;
-    height: 210px;
-  }
-  .title-part2 {
-    padding-left: 322px;
-  }
-  .tab-title {
-    margin: 30px 0px 80px 0px;
-  }
-  .desc {
-    width: 717px;
+/* Mobile size: 320px to 767px */
+@media (min-width: 320px) and (max-width: 767px) {
+  /* Adjust styles for mobile-sized screens */
+  /* Example styles */
+  .container {
+    position: relative;
   }
   .image-rugby {
-    padding: 0px 175px;
-    max-width: 100%;
-    height: auto;
+    width: 200px;
+    height: 281px;
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 74px;
   }
   .image-basketball {
-    right: 125px;
-    max-width: 100%;
-    height: auto;
+    width: 382px;
+    height: 249px;
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 95px;
+  }
+  .tab-title {
+    height: 20px;
+    margin-top: 20px;
+  }
+  .title {
+    padding: 0px 20px;
+  }
+  .tab1 {
+    padding: 15px 20px;
+  }
+  .tab2 {
+    padding: 15px 20px;
+  }
+  .tab3 {
+    padding: 60px 20px;
+    background: #F5F4F9;
+  }
+  .tab1-part2 {
+     padding: 15px 20px;
+  }
+  .tab2-part2 {
+     padding: 15px 20px;
+  }
+  .tab3-part2 {
+     padding: 60px 20px;
+     background: #F5F4F9;
   }
 }
 
-@media only screen and (max-width: 1919px) and (min-width: 769px) {
-  /* ปรับแต่งสไตล์สำหรับขนาดหน้าจอ 768px - 1920px */
+/* Tablet size: 768px to 1919px */
+@media (min-width: 768px) and (max-width: 1919px) {
+  /* Adjust styles for tablet-sized screens */
+  /* Example styles */
   .container {
     overflow-x: hidden;
-    overflow-y: auto; 
+    overflow-y: auto;
   }
   .image-rugby {
     width: 518px;
     height: 699px;
     position: absolute;
-    z-index: 1;
+    z-index: 2;
     left: -180px;
     top: 90px;
   }
   .image-basketball {
     width: 691px;
     height: 568px;
+    position: absolute;
     z-index: 2;
     right: -200px;
   }
@@ -358,13 +360,54 @@ html {
   }
 }
 
-@media only screen and (max-width: 768px) and (min-width: 321px) {
-  /* ปรับแต่งสไตล์สำหรับขนาดหน้าจอ 320px - 768px */
-  /* ... */
-}
-
-@media only screen and (max-width: 320px) {
-  /* ปรับแต่งสไตล์สำหรับขนาดหน้าจอน้อยกว่าหรือเท่ากับ 320px */
-  /* ... */
+/* Desktop size: 1920px and larger */
+@media (min-width: 1920px) {
+  /* Adjust styles for larger screens (Desktop) */
+  /* Example styles */
+  .title {
+    padding-left: 1008px;
+  }
+  .tab1 {
+    padding-left: 1008px;
+  }
+  .tab2 {
+    padding-left: 1008px;
+  }
+  .tab3 {
+    padding: 60px 0px 60px 1008px;
+  }
+  .tab1-part2 {
+    padding-left: 322px;
+  }
+  .tab2-part2 {
+    padding-left: 322px;
+  }
+  .tab3-part2 {
+    padding: 60px 0px 108px 322px;
+    height: 210px;
+  }
+  .title-part2 {
+    padding-left: 322px;
+  }
+  .tab-title {
+    margin: 30px 0px 80px 0px;
+  }
+  .desc {
+    width: 717px;
+  }
+  .image-rugby {
+    padding: 0px 175px;
+    max-width: 100%;
+    height: auto;
+    position: absolute;
+    z-index: 2;
+  }
+  .image-basketball {
+    right: 125px;
+    max-width: 100%;
+    height: auto;
+    position: absolute;
+    z-index: 2;
+  }
 }
 </style>
